bf97ae00ca86022b36305606c772c496e43361d5
Gulda96 <t.gulda@seznam.cz>
2020-02-19 16:43:02 +0100

    final cv3

--- cv03/Src/main.c
+++ cv03/Src/main.c
@@ -12,113 +12,24 @@
 #endif
 
 #include "stm32f0xx.h"
-#define  LED_TIME_BLINK 300
-#define  LED_TIME_SHORT 100
-#define  LED_TIME_LONG  1000
-#define  PIN_READ_TIME 5
-
-volatile uint32_t Tick;
-
-void EXTI0_1_IRQHandler(void)
-{
-	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
-
-		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
-		GPIOB->ODR ^= (1<<0); // toggle;
-	}
-}
-
-void SysTick_Handler(void)
-{
-	Tick++;
-}
-
-void blikac(void)
-{
-	static uint32_t delay;
-
-	if (Tick > delay + LED_TIME_BLINK) {
-		GPIOA->ODR ^= (1<<4);
-		delay = Tick;
-	}
-}
-
-void tlacitka(void)
-{
-	//static uint32_t old_s2;
-	static uint32_t off_time;
-	//static uint32_t old_s1;
-	static uint16_t debounce = 0xFFFF;
-	static uint32_t check_time;
-	static uint32_t check_time2;
+#include "sct.h"
 
 
-
-	//uint32_t new_s2 = GPIOC->IDR & (1<<1);
-	//uint32_t new_s1 = GPIOC->IDR & (1<<0);
-
-	if(Tick > check_time + PIN_READ_TIME)
-	{
-		debounce <<= 1;
-		if(GPIOC->IDR & (1<<0)){
-			debounce |= 0x0001;
-		}
-		if(debounce == 0x7FFF){
-			off_time = Tick + LED_TIME_SHORT;
-			GPIOB->BSRR = (1<<0);
-		}
-		check_time = Tick;
-	}
-
-	if(Tick > check_time2 + PIN_READ_TIME)
-	{
-		debounce <<= 1;
-		if(GPIOC->IDR & (1<<1)){
-			debounce |= 0x0001;
-		}
-		if(debounce == 0x7FFF){
-			off_time = Tick + LED_TIME_LONG;
-			GPIOB->BSRR = (1<<0);
-		}
-		check_time2 = Tick;
-	}
-
-	/*if (old_s2 && !new_s2) { // falling edge
-		off_time = Tick + LED_TIME_LONG;
-		GPIOB->BSRR = (1<<0);
-	}
-	old_s2 = new_s2;*/
-
-	/*if (old_s1 && !new_s1) { // falling edge
-		off_time = Tick + LED_TIME_SHORT;
-		GPIOB->BSRR = (1<<0);
-	}
-	old_s1 = new_s1;*/
-
-	if (Tick > off_time) {
-		GPIOB->BRR = (1<<0);
-	}
-
-}
 
 int main(void)
 {
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
-	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
-	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
-	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
-	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
-
-	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
-	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
-	//EXTI->IMR |= EXTI_IMR_MR0; // mask
-	//EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
-	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
-
-	SysTick_Config(8000);
+	sct_init();
+	sct_led(0x7A5C36DE);
 
 	for(;;){
-		blikac();
-		tlacitka();
+
+		for(uint32_t i = 0; i<1000; i +=111 ){
+
+			sct_value(i);
+			for(uint32_t j=0; j<1000000; j++){}
+		}
+
+
 	}
+
 }
